6f9f67695b7d46dc4a5563766dbe7a147ae0edfb
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-01-24 22:23:51 +0700

    ADD:searchCity()function(Retrieve city input and get the weather data)

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
 /**
- * Retrieve (fetch) weather data from openweathermap API
+ * Function To Retrieve (fetch) weather data from openweathermap API
  * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
  */
 
@@ -10,24 +10,27 @@ const getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
   const weatherPromise = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      // throw new Error(response.statusText);
+      throw new Error("Something went wrong");
+    }
+  });
+};
 
-  return weatherPromise
-    .then((respone) => {
-      if (respone.ok) {
-        // console.log(respone);
-        // console.log(respone.json());
-        return respone.json();
-      } else {
-        // throw new Error(response.statusText);
-        throw new Error("Something went wrong");
-      }
-    })
-    .then((data) => {
-      console.log(data);
+/**
+ * Function To Retrieve city input and get the weather data
+ */
+
+const searchCity = () => {
+  const city = document.getElementById("city-input").value;
+  getWeatherData(city)
+    .then((response) => {
+      console.log(response);
     })
     .catch((error) => {
       console.log("Fetch Error :-S", error);
     });
 };
-
-getWeatherData("detroit");
