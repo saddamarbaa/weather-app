e00fb8a2f4e3d7f675f027aae3e8945b1c381860
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-01-24 21:38:39 +0700

    ADD:getWeatherData()function (fetch data from API)

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-/**
- * Weather App
- * TODO: Complete getWeatherData() to return json response Promise
- * TODO: Complete searchCity() to get user input and get data using getWeatherData()
- * TODO: Complete showWeatherData() to set the data in the the html file from response
- */
-
 // API_KEY for maps api
-let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+const API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
 /**
- * Retrieve weather data from openweathermap
- * HINT: Use fetch()
- * HINT: URL should look like this:
+ * Retrieve (fetch) weather data from openweathermap API
  * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
  */
-getWeatherData = (city) => {
-  const URL = "https://api.openweathermap.org/data/2.5/weather";
-  //HINT: Use template literals to create a url with input and an API key
 
-  //CODE GOES HERE
+const getWeatherData = (city) => {
+  const URL = "https://api.openweathermap.org/data/2.5/weather";
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+  const weatherPromise = fetch(FULL_URL);
+
+  return weatherPromise
+    .then((respone) => {
+      if (respone.ok) {
+        // console.log(respone);
+        // console.log(respone.json());
+        return respone.json();
+      } else {
+        // throw new Error(response.statusText);
+        throw new Error("Something went wrong");
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log("Fetch Error :-S", error);
+    });
 };
+
+getWeatherData("detroit");
